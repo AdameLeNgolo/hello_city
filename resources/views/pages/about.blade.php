0ea3495ecdbab4b19e7aeec1a99b7ac4f72d75b0
adamelengolo <holndou359@gmail.com>
2021-03-26 12:18:46 +0000

    Add images

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -3,6 +3,8 @@
 @section('title','About Us | '.config('app.name'))
     
 @section('content')
+  <img src="/img/dylan.jpg" alt="Dylan O'Brian">
+
   <p>Build with &hearts; by lengolo form les Teachers DU NET </p>
 
       <p><a href="{{ route('home') }}">Retour vers la page d'accueil</a></p>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,8 +2,10 @@
 
 
 @section('content')
-      <h1>Hello from Senegal</h1>
+    <img src="/img/senegal.jpg" alt="drapeau du senegal">
 
-      <p>It's currently {{ date('H:i') }}.</p>
+     <h1>Hello from Senegal</h1>
+
+     <p>It's currently {{ date('H:i') }}.</p>
 
 @endsection
