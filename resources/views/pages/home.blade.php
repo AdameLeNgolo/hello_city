2ef5a55b2d4aa14d50731a4a0abbbae1aee85f12
adamelengolo <holndou359@gmail.com>
2021-03-27 02:38:06 +0000

    Add seconds part

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -6,6 +6,6 @@
 
      <h1 class="mt-5 font-semibold text-3xl sm:text-5xl text-indigo-600">Hello from Senegal</h1>
 
-     <p class="text-lg text-gray-800">It's currently {{ date('H:i') }}.</p>
+     <p class="text-lg text-gray-800">It's currently {{ date('H:i:s') }}.</p>
 
 @endsection
